d846cdef7cd44b02d1cc0a1c37cb75cb0f071792
Crest Data Systems <60967033+crestdatasystems@users.noreply.github.com>
2023-03-30 10:36:12 +0530

    Initial app submission (#1)
    * Initial app submission
    
    * fix static failure
    
    * Made suggested changes
    
    ---------
    
    Co-authored-by: Dhwani Shah <dhwani.shah@CDSYS.LOCAL>
    Co-authored-by: Parv Desai <pdesai@splunk.com>

---
+++ dataminrpulse_consts.py
@@ -0,0 +1,77 @@
+# File: dataminrpulse_consts.py
+#
+# Copyright (c) 2023 Dataminr
+#
+# This unpublished material is proprietary to Dataminr.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Dataminr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+# Endpoints
+DATAMINRPULSE_BASE_URL = "https://gateway.dataminr.com"
+DATAMINRPULSE_GET_LISTS = "/account/2/get_lists?alertversion=14"
+DATAMINRPULSE_GET_ALERTS = "/api/3/alerts?alertversion=14"
+DATAMINRPULSE_GET_RELATED_ALERTS = "/alerts/2/get_related?alertversion=14"
+DATAMINRPULSE_ENDPOINT_TOKEN = "/auth/2/token"
+DATAMINRPULSE_STATE_TOKEN = "token"
+DATAMINRPULSE_STATE_DMA_TOKEN = "dmaToken"
+DATAMINRPULSE_STATE_REFRESH_TOKEN = "refreshToken"
+DATAMINRPULSE_STATE_EXPIRE = "expire"
+DATAMINRPULSE_ERROR_STATE_FILE_CORRUPT = "Error occurred while loading the state file. " \
+                                         "Resetting the state file with the default format"
+DATAMINRPULSE_REQUEST_TIMEOUT = 240
+DATAMINRPULSE_ENDPOINT_TEST_CONNECTIVITY = "/auth/2/token"
+DATAMINRPULSE_ERROR_SYSTEM_HEALTH = "Failed to get the system health status"
+DATAMINRPULSE_ACTION_EMPTY_ALERTS_RESPONSE = "No alerts found"
+DATAMINRPULSE_ACTION_EMPTY_RELATED_ALERTS_RESPONSE = "No related alerts found"
+DATAMINRPULSE_ERROR_TEST_CONNECTIVITY_INVALID_STATUS = "Error message: Server status is {}"
+DATAMINRPULSE_SUCCESS_TEST_CONNECTIVITY = "Test Connectivity Passed"
+DATAMINRPULSE_ERROR_TEST_CONNECTIVITY = "Test Connectivity Failed"
+DATAMINRPULSE_ERROR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+DATAMINRPULSE_ERROR_GENERAL_MSG = "Status code: {0}, Data from server: {1}"
+DATAMINRPULSE_ERROR_INVALID_INT_PARAM = "Please provide a valid integer value in the '{key}' parameter"
+DATAMINRPULSE_ERROR_NEGATIVE_INT_PARAM = "Please provide a valid non-negative integer value in the '{key}' parameter"
+DATAMINRPULSE_ERROR_ZERO_INT_PARAM = "Please provide a non-zero positive integer value in the '{key}' parameter"
+DATAMINRPULSE_STATE_FROM_VALUE = 'from'
+DATAMINRPULSE_STATE_TO_VALUE = 'to'
+DATAMINRPULSE_STATE_LIST_ID_VALUE = 'list_id'
+DATAMINRPULSE_STATE_QUERY_VALUE = 'query'
+DATAMINRPULSE_ERROR_JSON_RESPONSE = "Unable to parse JSON response. Error: {0}"
+DATAMINRPULSE_ERROR_REST_CALL = "Error connecting to server. Details: {0}"
+DATAMINRPULSE_ERROR_EMPTY_RESPONSE = "Status code: {}. Empty response and no information available"
+DATAMINRPULSE_ERROR_HTML_RESPONSE = "Error parsing html response"
+DATAMINRPULSE_DUPLICATE_CONTAINER_FOUND_MSG = "duplicate container found"
+DATAMINRPULSE_ACTION_EMPTY_ALERTS = 'No alerts found'
+DATAMINRPULSE_ALERT_TYPE_VALUE = ['All', 'Alert', 'Urgent', 'Flash']
+DATAMINRPULSE_ERROR_INVALID_PARAMETER_VALUE = "Please provide valid value in the 'Alert Type' asset parameter"
+DATAMINRPULSE_APP_ID = '8630b723-b317-4765-b923-5be5229c71d1'
+DATAMINRPULSE_EMPTY_RESPONSE_STATUS_CODE = [200, 201, 204]
+DATAMINRPULSE_DECODE_FROM_ERROR = 'Error from server. Error message: Unable to decode "from" cursor, invalid format'
+DATAMINRPULSE_SEVERITY_ENDPOINT = 'rest/severity'
+DATAMINRPULSE_SEVERITY_PAYLOAD = [
+    {
+        "color": "red",
+        "name": "Flash"
+    },
+    {
+        "color": "orange",
+        "name": "Urgent"
+    },
+    {
+        "color": "yellow",
+        "name": "Alert"
+    }
+]
---
+++ tests/test_dataminrpulse_test_connectivity.py
@@ -0,0 +1,172 @@
+# File: test_dataminrpulse_test_connectivity.py
+#
+# Copyright (c) 2023 Dataminr
+#
+# This unpublished material is proprietary to Dataminr.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Dataminr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+# File: test_dataminrpulse_get_alerts.py
+#
+# Copyright (c) 2023 Dataminr
+#
+# This unpublished material is proprietary to Dataminr.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Dataminr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+
+import json
+import unittest
+from unittest.mock import patch
+
+import dataminrpulse_consts as consts
+from dataminrpulse_connector import DataminrPulseConnector
+
+from . import dataminrpulse_config
+
+
+class TestConnectivityAction(unittest.TestCase):
+    """Class to test the Test Connectivity action."""
+
+    def setUp(self):
+        """Set up method for the tests."""
+        self.connector = DataminrPulseConnector()
+        self.test_json = dict(dataminrpulse_config.TEST_JSON)
+        self.test_json.update({"action": "test connectivity", "identifier": "test_connectivity"})
+
+        return super().setUp()
+
+    @patch("dataminrpulse_utils.requests.post")
+    def test_connectivity_pass(self, mock_post):
+        """
+        Test the valid case for the test connectivity action.
+
+        Patch the post() to return valid token.
+        """
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.headers = dataminrpulse_config.DEFAULT_HEADERS
+        mock_post.return_value.json.return_value = {"dmaToken": "dummy_token", "refreshToken": "dummy_refresh_token", "expire": "expire"}
+
+        ret_val = self.connector._handle_action(json.dumps(self.test_json), None)
+        ret_val = json.loads(ret_val)
+        self.assertEqual(ret_val['result_summary']['total_objects'], 1)
+        self.assertEqual(ret_val['result_summary']['total_objects_successful'], 1)
+        self.assertEqual(ret_val['status'], 'success')
+
+        mock_post.assert_called_with(
+            f'https://gateway.dataminr.com{consts.DATAMINRPULSE_ENDPOINT_TOKEN}',
+            headers=dataminrpulse_config.TOKEN_HEADER,
+            data=dataminrpulse_config.TOKEN_DATA,
+            params=None,
+            timeout=consts.DATAMINRPULSE_REQUEST_TIMEOUT,
+            verify=False,
+        )
+
+    @patch("dataminrpulse_utils.requests.post")
+    def test_connectivity_token_bad_credentials_fail(self, mock_post):
+        """
+        Test the fail case for the test connectivity action.
+
+        Patch the post() to return authentication error.
+        """
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.headers = dataminrpulse_config.DEFAULT_HEADERS
+        mock_post.return_value.json.return_value = {"errors": [{"code": 103, "message": "Authentication error. Invalid token"}]}
+        ret_val = self.connector._handle_action(json.dumps(self.test_json), None)
+        ret_val = json.loads(ret_val)
+        self.assertEqual(ret_val['result_summary']['total_objects'], 1)
+        self.assertEqual(ret_val['result_summary']['total_objects_successful'], 0)
+        self.assertEqual(ret_val['status'], 'failed')
+
+        mock_post.assert_called_with(
+            f'https://gateway.dataminr.com{consts.DATAMINRPULSE_ENDPOINT_TOKEN}',
+            headers=dataminrpulse_config.TOKEN_HEADER,
+            data=dataminrpulse_config.TOKEN_DATA,
+            params=None,
+            timeout=consts.DATAMINRPULSE_REQUEST_TIMEOUT,
+            verify=False,
+        )
+
+    @patch("dataminrpulse_utils.requests.post")
+    def test_connectivity_client_secret_error_fail(self, mock_post):
+        """
+        Test the fail case for the test connectivity action.
+
+        Patch the post() to return response with incorrect client secret.
+        """
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.headers = dataminrpulse_config.DEFAULT_HEADERS
+        mock_post.return_value.json.return_value = {"errors": [{"code": 102, "message": "Invalid client Id or client secret"}]}
+
+        self.test_json['config']['client_secret'] = dataminrpulse_config.CLIENT_SECRET_DUMMY_ENCRYPTED
+        ret_val = self.connector._handle_action(json.dumps(self.test_json), None)
+        ret_val = json.loads(ret_val)
+        self.assertEqual(ret_val['result_summary']['total_objects'], 1)
+        self.assertEqual(ret_val['result_summary']['total_objects_successful'], 0)
+        self.assertEqual(ret_val['status'], 'failed')
+
+        dataminrpulse_config.TOKEN_DATA['client_secret'] = dataminrpulse_config.CLIENT_DUMMY_ACTUAL
+        mock_post.assert_called_with(
+            f'https://gateway.dataminr.com{consts.DATAMINRPULSE_ENDPOINT_TOKEN}',
+            timeout=consts.DATAMINRPULSE_REQUEST_TIMEOUT,
+            verify=False,
+            params=None,
+            data=dataminrpulse_config.TOKEN_DATA,
+            headers=dataminrpulse_config.TOKEN_HEADER,
+        )
+
+    @patch("dataminrpulse_utils.requests.post")
+    def test_connectivity_client_id_error_fail(self, mock_post):
+        """
+        Test the fail case for the test connectivity action.
+
+        Patch the post() to return response with incorrect client id.
+        """
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.headers = dataminrpulse_config.DEFAULT_HEADERS
+        mock_post.return_value.json.return_value = {"errors": [{"code": 102, "message": "Invalid client Id or client secret"}]}
+
+        self.test_json['config']['client_id'] = dataminrpulse_config.CLIENT_DUMMY_ACTUAL
+        ret_val = self.connector._handle_action(json.dumps(self.test_json), None)
+        ret_val = json.loads(ret_val)
+        self.assertEqual(ret_val['result_summary']['total_objects'], 1)
+        self.assertEqual(ret_val['result_summary']['total_objects_successful'], 0)
+        self.assertEqual(ret_val['status'], 'failed')
+
+        dataminrpulse_config.TOKEN_DATA['client_id'] = dataminrpulse_config.CLIENT_DUMMY_ACTUAL
+        mock_post.assert_called_with(
+            f'https://gateway.dataminr.com{consts.DATAMINRPULSE_ENDPOINT_TOKEN}',
+            timeout=consts.DATAMINRPULSE_REQUEST_TIMEOUT,
+            verify=False,
+            params=None,
+            data=dataminrpulse_config.TOKEN_DATA,
+            headers=dataminrpulse_config.TOKEN_HEADER,
+        )
