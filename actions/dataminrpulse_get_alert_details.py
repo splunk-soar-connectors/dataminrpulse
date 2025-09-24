1ed486cdf2852b37f8ad0e53f725161f2f0ae3c1
Crest Data <it.systems@crestdata.ai>
2025-09-24 16:27:32 +0530

    Handle get alert details with new endpoint

--- actions/dataminrpulse_get_alert_details.py
+++ actions/dataminrpulse_get_alert_details.py
@@ -39,44 +39,32 @@ class GetAlertDetailsAction(BaseAction):
             return self._action_result.set_status(phantom.APP_ERROR, "Please use the latest v4 API version to perform this action")
 
         if alert_id:
-            params = {"query": f"{alert_id.split('-')[0]}"}
             ret_val, response = self._connector.util._make_rest_call_helper(
-                consts.DATAMINRPULSE_GET_ALERTS_V4, self._action_result, params=params
+                consts.DATAMINRPULSE_GET_ALERT_V4.format(alert_id=alert_id), self._action_result
             )
             if phantom.is_fail(ret_val):
                 return self._action_result.get_status()
+
+            if response:
+                # Handle metadata field as a list to maintain consistency with the response received when fetching from artifact ID
+                if response.get("metadata"):
+                    response["metadata"] = [response["metadata"]]
+                self._action_result.add_data(response)
+            else:
+                return self._action_result.set_status(phantom.APP_ERROR, "No alert details found")
+            
         elif artifact_id:
             base_url = self._connector._get_phantom_base_url()
-
             url = consts.DATAMINRPULSE_GET_ARTIFACT_DETAILS.format(instance=base_url, artifact_id=artifact_id)
             ret_val, response = self._connector.util._make_rest_call(url, self._action_result)
             if phantom.is_fail(ret_val):
                 return self._action_result.get_status()
             response = response.get("cef", {})
+            if response:
+                self._action_result.add_data(response)
+            else:
+                return self._action_result.set_status(phantom.APP_ERROR, "No alert details found")
         else:
             return self._action_result.set_status(phantom.APP_ERROR, "Please provide either artifact_id or alert_id")
-
-        # Handle both single alert and alerts array response
-        if isinstance(response, dict):
-            if "alerts" in response:
-                alert_details = response["alerts"]
-            else:
-                alert_details = response  # Single alert response
-        elif isinstance(response, list):
-            alert_details = response
-        else:
-            alert_details = []
-
-        # Add each alert as separate data item
-        if alert_details:
-            if isinstance(alert_details, list):
-                for alert in alert_details:
-                    if alert.get("metadata"):
-                        alert["metadata"] = [alert["metadata"]]
-                    self._action_result.add_data(alert)
-            else:
-                self._action_result.add_data(alert_details)
-        else:
-            return self._action_result.set_status(phantom.APP_ERROR, "No alert details found")
-
+        
         return self._action_result.set_status(phantom.APP_SUCCESS, "Successfully fetched alert details")
